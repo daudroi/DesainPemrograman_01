9c77b2e2f93bcd690f4c774ca482bb7e0f8a348c
daudroi <achmaddaud42@gmail.com>
2025-09-23 16:09:57 +0700

    soal no 3.2

--- Jobsheet4/operator.php
+++ Jobsheet4/operator.php
@@ -16,3 +16,19 @@ echo "Hasil Kali: {$hasilKali} <br>";
 echo "Hasil Bagi: {$hasilBagi} <br>";
 echo "Sisa Bagi: {$sisaBagi} <br>";
 echo "Pangkat: {$pangkat} <br>";
+
+//soal no 3.2
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "<br>";
+echo "Hasil Sama: " . ($hasilSama ? 'true' : 'false') . "<br>";
+echo "Hasil Tidak Sama: " . ($hasilTidakSama ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Kecil: " . ($hasilLebihKecil ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Besar: " . ($hasilLebihBesar ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Kecil Sama: " . ($hasilLebihKecilSama ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Besar Sama: " . ($hasilLebihBesarSama ? 'true' : 'false') . "<br>";
