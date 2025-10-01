aa4875c71f5a596e1dd3eb1e537a0814a9c9d9c6
daudroi <achmaddaud42@gmail.com>
2025-10-01 09:00:15 +0700

    return part 2

--- Jobsheet5/function_return.php
+++ Jobsheet5/function_return.php
@@ -3,5 +3,12 @@ function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
-echo "Umur saya adalah " . hitungUmur(2005, 2025) . " tahun";
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Saya berusia ". hitungUmur(2005, 2025) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan("Daud");
 ?>
