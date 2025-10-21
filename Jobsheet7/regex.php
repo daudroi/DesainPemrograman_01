039cfb6fc890806c4115b6eabb849ae11b64b49c
daudroi <achmaddaud42@gmail.com>
2025-10-21 08:24:46 +0700

    mencari teks dengan huruf g diikuti satu atau dua huruf o dan diakhiri huruf d, seperti "god" atau "good"

--- Jobsheet7/regex.php
+++ Jobsheet7/regex.php
@@ -35,4 +35,11 @@ if (preg_match($pattern, $text, $matches)) {
 } else {
     echo "Tidak ada yang cocok!<br>";
 }
+$pattern = '/go{1,2}d/';   // huruf "o" harus muncul 1 sampai 2 kali
+$text = 'gd god good goood';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
