3f87f4e4d54ad83b9a88a56a5689466d07484315
daudroi <achmaddaud42@gmail.com>
2025-10-21 07:44:51 +0700

    penggunaan isset 2

--- Jobsheet7/isset.php
+++ Jobsheet7/isset.php
@@ -7,4 +7,10 @@ if (isset($umur) && $umur >= 18) {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.<br>";
 }
 
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
 ?>
